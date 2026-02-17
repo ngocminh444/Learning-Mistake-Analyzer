158aaeb097bd6cae764cf5921d934d9da6d91b30
ngocminh444 <ngocminhdinh113@gmail.com>
2026-02-17 17:04:32 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,7 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-# load API key
-load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 app = Flask(__name__)
 CORS(app)
